3c40ec172ad0f7737ae7b74931ecb9f6cc30beca
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-11-24 20:52:11 +0100

    Delete

---
+++ delete.py
@@ -0,0 +1,23 @@
+import connect
+
+# Sentencia sql
+sql = f'DELETE FROM personas WHERE id_persona= %s'
+
+# Solicitar datos al Usuario
+id_persona = input('Ingrese el id del registro a eliminar: ')
+
+# Usar metodo execute
+connect.cur.execute(sql,id_persona)
+
+# Guardar cambios
+connect.conn.commit()
+
+# Contador de registros eliminados
+registro_eliminado = connect.cur.rowcount
+
+# Mensaje al Usuario
+print(f'Registro eliminado: {registro_eliminado}')
+
+# Cerrar cursor
+connect.cur.close()
+
